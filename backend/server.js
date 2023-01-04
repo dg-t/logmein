17f9194317fbb8bc03da68b0a1b1404d519c02a1
DG-T <orione_90@hotmail.com>
2023-01-04 18:32:07 +0100

    Connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./index');
 
 dotenv.config({ path: './config.env' });
+
+mongoose.set('strictQuery', false);
+(async () => {
+	try {
+		await mongoose.connect(process.env.DATABASE, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		// eslint-disable-next-line no-console
+		console.log('Connected to DB!!!');
+	} catch (err) {
+		// eslint-disable-next-line no-console
+		console.log(`Connection to DB failed! err: ${err}`);
+	}
+})();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
