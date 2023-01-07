c2fefe99890839f127d0ca0d8c9e36d0f6807903
DG-T <orione_90@hotmail.com>
2023-01-07 12:44:44 +0100

    improve app security - send jwt with cookie - set a rate limit for all api andepoint and for login endpoint - add http header security - implement data sanitization - prevent parameter pollutions

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,6 +14,17 @@ const jwtSign = (id) =>
 const createSendToken = (user, statusCode, res, sendUser) => {
 	const token = jwtSign(user._id);
 
+	const cookieOptions = {
+		expires: new Date(
+			Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+		),
+		httpOnly: true,
+	};
+	if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+	res.cookie('jwt', token, cookieOptions);
+
+	user.password = undefined;
+
 	res.status(statusCode).json({
 		status: 'success',
 		token,
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -7,8 +12,39 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 // MIDDLEWARE
-app.use(express.json());
+// HTTP Security
+app.use(helmet());
+
+// login info during development
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// limit requests
+const appLimiter = rateLimit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many request. Try again in one hour',
+});
+const loginLimiter = rateLimit({
+	max: 5,
+	windowMs: 30 * 60 * 1000,
+	message: 'Too many request. Try again in 30 min',
+});
+app.use('/api', appLimiter);
+app.use('/api/v1/users/login', loginLimiter);
+
+// Read data from body - we limit to 10 kb (optional)
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization
+app.use(mongoSanitize()); // NoSQL query injection
+app.use(xss()); // Cross-Site Scripting
+
+/*
+ * Prevent parameter pollution - will only allow one URL parameter (last one) removing duplicates
+ * If need to allow URL parameters to be repeated for certain parameter, need to whitelist parameters
+ * To whitelist parameter: app.use(hpp({whitelist: ['paramName1', 'paramName2']}));
+ */
+app.use(hpp());
 
 // ROUTES
 app.use('/api/v1/users', userRouter);
