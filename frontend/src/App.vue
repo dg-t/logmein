d7962611fff9676fa4332f75745ec625c8c05781
DG-T <orione_90@hotmail.com>
2023-01-29 16:47:03 +0100

    Create AppAuthentication view and LoginForm component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="row">
-    <h1 class="col-6">Hello Vue</h1>
-    <div
-      class="col-6 p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3"
-    >
-      Example element with utilities
-    </div>
+  <div>
+    <AppAuthentication />
   </div>
 </template>
 
-<script setup></script>
+<script>
+import AppAuthentication from './views/authentication/AppAuthentication.vue';
+export default {
+  name: 'App',
+  components: {
+    AppAuthentication,
+  },
+};
+</script>
 
 <style scoped></style>
---
+++ frontend/src/views/authentication/AppAuthentication.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+import LoginForm from '../../components/authentication/LoginForm.vue';
+export default {
+  name: 'AppAuthentication',
+  components: {
+    LoginForm,
+  },
+};
+</script>
+
+<style scoped></style>
